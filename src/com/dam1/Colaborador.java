d9a5ebbe016df10567e9e49f95efb695a182a9f0
Adrian Martinez <amartinezpineiro@danielcastelao.org>
2022-01-26 14:46:10 +0100

    comit del colaborador en la rama main

---
+++ src/com/dam1/Colaborador.java
@@ -0,0 +1,8 @@
+package com.dam1;
+
+public class Colaborador {
+    //Hagan lo que hagan, trabajen de buena gana, como para el Señor y no como para nadie en este mundo, conscientes de que el Señor los recompensará con la herencia. Ustedes sirven a Cristo el Señor.
+    //Colosenses 3:23-24
+    //viva cristo rey
+    //amaras al progimo como a ti mismo
+}
